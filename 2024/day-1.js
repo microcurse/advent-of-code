b48ffdb9f2fcdb6c3d9212a605c32e004aa061b6
Marc <marc.maninang@gmail.com>
2024-12-01 22:54:26 -0800

    Part two completed

--- 2024/day-1.js
+++ 2024/day-1.js
@@ -40,9 +40,42 @@
  * - Repeat until there are no numbers, appending each result to the result variable.
  * - Output the total distance.
  *  
+ * 
+ * Part Two
+ * 
+ * Figure out exactly how often each number from the left list appears in the right list. Calculate a total "similarity score" by adding up each number in the left list after multiplying it by the number of times that number appears in the right list.
+ * 
+ * Explicit Requirements:
+ * - Input: Two lists (arrays) of numbers.
+ * - Output: A number representing the total "similarity score".
+ * - A "similarity score" is calculated by multiplying a number from the left list by the number of times it appears in the right list.
+ * - Add up all "similarity scores" to receive a total for output.
+ * 
+ * Implicit Requirements:
+ * - If a number from the left list appears 0 times in the right list, then it's multiplied by 0.
+ * - Don't worry about sorting.
+ * 
+ * Examples and Test Cases:
+ * list1 = [3, 4, 2, 1, 3, 3]
+ * list2 = [4, 3, 5, 3, 9, 3]
+ * 
+ * First number 3 appears 3 times in list2 -> similarity score = 3 * 3 = 9
+ * Second number 4 appears 1 time in list2 -> similarity score = 4 * 1 = 4
+ * etc..
+ * 
+ * Algorithm
+ * - Iterate over the list1 and store the current number into a variable
+ * - Create a variable and store the number of times the current number occurs in list2
+ *  - Iterate over list2 and increment a count by 1 for each occurance.
+ *  - Repeat until all numbers have been iterated over.
+ *  - Return the count.
+ * - Multiply the current number by the number of occurances and store into a similarity score variable
+ * - Repeat until all numbers are iterated over
+ * - Output the similarity score
+ * 
  */
 
-fs = require('fs');
+const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('day-1.json'));
 const list1 = data.list1;
 const list2 = data.list2;
@@ -61,4 +94,23 @@ const findTheDistanceBetweenLists = (list1, list2) => {
   return result;
 }
 
-console.log(findTheDistanceBetweenLists(list1, list2));
+const calculateSimilarityScore = (list1, list2) => {
+  let similarityScore = 0;
+
+  list1.forEach((num) => {
+    similarityScore += num * numOfOccurences(num, list2);
+  });
+
+  return similarityScore;
+}
+
+const numOfOccurences = (num, list) => {
+  let count = 0;
+  list.forEach((num2) => {
+    if (num === num2) count++;
+  })
+  return count;
+}
+
+// console.log(findTheDistanceBetweenLists(list1, list2));
+console.log(calculateSimilarityScore(list1, list2));
